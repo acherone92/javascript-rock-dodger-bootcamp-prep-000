cbb8d6c29cb0d4466207f4fd9c2677b3586c0d99
Anthony C <acherone92@gmail.com>
2018-05-21 16:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function endGame() {
   
   for (i=0; i<ROCKS.length; i++){
     console.log(game.querySelector('.rock'));
-    game.removeChild(game.rock.childNodes([o]));
+    game.removeChild(game.childNodes);
     console.log('for');
     console.log(game);  
     debugger;

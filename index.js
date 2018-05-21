b277b15a3000e41ccbe1470c28267cc5bc5bddc1
Anthony C <acherone92@gmail.com>
2018-05-21 14:46:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ function endGame() {
   console.log('game-over');
   clearInterval(gameInterval);
   console.log(document.getElementsByClassName('rock'));
-  document.getElementsByClassName('rock').removeChild('.rock');
+  document.getElementsByClassName('rock').removeChild();
   console.log(document.getElementsByClassName('rock'));
   window.removeEventListener('keydown', moveDodger);
   alert('YOU LOSE!');

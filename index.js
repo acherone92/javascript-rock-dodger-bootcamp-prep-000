1af551de7e24a434c757027a5de6491c248be462
Anthony C <acherone92@gmail.com>
2018-05-21 17:42:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,14 +69,15 @@ function createRock(x) {
     // implement me!
     rock.style.top = `${top += 2}px`;
     console.log('top ' + top);
-    if (top > GAME_HEIGHT) {
+    if (top < GAME_HEIGHT){
+    window.requestAnimationFrame(moveRock);
+    } else {
+      if (top > GAME_HEIGHT) {
       console.log('removed' + game.querySelector('.rock'));
       game.removeChild(rock);
       debugger;
-      }
-    if (top < GAME_HEIGHT){
-    window.requestAnimationFrame(moveRock);
         }
+    
     if (checkCollision(rock)===true){
       endGame();
     }
